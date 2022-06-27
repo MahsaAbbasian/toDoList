03c4af02eb311ca82aba44a3caf6c58afffe4bf2
Moeinian <sadegh.moeinian@midroc.se>
2022-06-27 20:56:31 +0200

    This is the first vesion of our code

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("hello");
+document.getElementById("listRow").innerHTML=`
+<div class="rowTask">
+    <div class="checkbox">
+        <input type="checkbox" onclick="taskdescription" class="taskBox">   
+    </div>
+    <div class="title">
+        Task1
+    </div>
+</div>
+`
+                
