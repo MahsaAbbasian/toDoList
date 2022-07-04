40b41e9246f99d9ddd20304ec138bdbd9fc35a3d
Moeinian <sadegh.moeinian@midroc.se>
2022-07-04 21:29:32 +0200

    adding to do list and change handler function to javascript code

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 console.log("hello");
-document.getElementById("listRow").innerHTML=`
+let activeTextValue = "";
+let toDoList = [];
+
+const changeToDoText = (textValue) => {
+  console.log(textValue);
+  activeTextValue = textValue;
+};
+
+const addToDo = () => {
+  toDoList.push(activeTextValue);
+  activeTextValue = "";
+  document.getElementById("activeTextInput").innerHTML = "";
+  console.log(toDoList);
+};
+
+document.getElementById("listRow").innerHTML = `
 <div class="rowTask">
     <div class="checkbox">
         <input type="checkbox" onclick="taskdescription" class="taskBox">   
@@ -7,6 +22,4 @@ document.getElementById("listRow").innerHTML=`
     <div class="title">
         Task1
     </div>
-</div>
-`
-                
+</div>`;
